6a129a972f921fa942f64e4fc1747641c90bcb59
denis <denis@Hoteia-Laptop.home>
2013-11-06 15:59:15 +0100

    for webapp-sso-test to webapp-sso-redirect-test and webapp-sso-standalone-test

---
+++ webapps/webapp-sso-standalone-test/src/main/java/org/hoteia/cas/config/RootConfig.java
@@ -0,0 +1,20 @@
+package org.hoteia.cas.config;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@ComponentScan(basePackages = { "org.hoteia.cas." })
+public class RootConfig {
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocation(new ClassPathResource("/application.properties"));
+		return ppc;
+	}
+	
+}
